674a77ca0468620c35b0c6434d2d0bc295c7411a
Mustafa <Mustafa.Haassen.dmm.k1@tuwaiq.edu.sa>
2021-10-28 10:41:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ratibbha/splash/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.example.ratibbha.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ratibbha.R
+
+class SplashActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+
+
+    }
+}
