09168b79ee1f45f6475ba4f8028b7a708927f465
Mustafa <Mustafa.Haassen.dmm.k1@tuwaiq.edu.sa>
2021-10-28 12:34:55 +0300

    added splash

--- app/src/main/java/com/example/ratibbha/splash/SplashActivity.kt
+++ app/src/main/java/com/example/ratibbha/splash/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.example.ratibbha.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import com.example.ratibbha.view.MainActivity
 import com.example.ratibbha.R
 
 class SplashActivity : AppCompatActivity() {
@@ -11,6 +14,19 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val splash = Intent(this, MainActivity::class.java)
+        object : CountDownTimer(4000,1000){
+
+            override fun onTick(p0: Long) {
+
+            }
+
+            override fun onFinish() {
+                startActivity(splash)
+            }
+
+        }.start()
 
 
 
--- app/src/main/java/com/example/ratibbha/view/MainActivity.kt
+++ app/src/main/java/com/example/ratibbha/view/MainActivity.kt
@@ -1,11 +1,25 @@
-package com.example.ratibbha
+package com.example.ratibbha.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ratibbha.R
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        val mainFragment = supportFragmentManager.findFragmentById(R.id.app_logo)
+
+        if (mainFragment == null){
+
+        }
+
+
     }
 }
