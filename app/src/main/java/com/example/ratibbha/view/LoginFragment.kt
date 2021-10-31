7bf51e7c522a54635e54cae838a98010bc35ceba
Mustafa <Mustafa.Haassen.dmm.k1@tuwaiq.edu.sa>
2021-10-31 10:26:42 +0300

    added navigation graph and transmission button to register fragment

--- app/src/main/java/com/example/ratibbha/view/LoginFragment.kt
+++ app/src/main/java/com/example/ratibbha/view/LoginFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.fragment.findNavController
 import com.example.ratibbha.R
 
 
@@ -21,6 +24,27 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
 
+        val userNameEditText: EditText? = view?.findViewById(R.id.username_edit_textview_login)
+        val passwadEditText: EditText? = view?.findViewById(R.id.passward_edit_textview_login)
+        val loginButton: Button? = view?.findViewById(R.id.signin_button_login)
+        val registerButton: Button? = view?.findViewById(R.id.register_button_register)
+
+
+        loginButton?.setOnClickListener {
+            val username = userNameEditText?.text.toString()
+            val passward = passwadEditText?.text.toString()
+
+        }
+
+        registerButton?.setOnClickListener {
+
+
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+    }
 }
--- app/src/main/java/com/example/ratibbha/view/RegisterFragment.kt
+++ app/src/main/java/com/example/ratibbha/view/RegisterFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.fragment.findNavController
 import com.example.ratibbha.R
 
 
@@ -18,4 +21,24 @@ class RegisterFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        val userNameEditText: EditText? = view?.findViewById(R.id.username_edit_textview_register)
+        val passwardEditText: EditText? = view?.findViewById(R.id.passward_edit_textview_register)
+        val emailEditText: EditText? = view?.findViewById(R.id.email_edit_textview_register)
+        val registerButton: Button? = view?.findViewById(R.id.register_button_register)
+
+
+        registerButton?.setOnClickListener {
+            val username = userNameEditText?.text.toString()
+            val email = emailEditText?.text.toString()
+            val passward = passwardEditText?.text.toString()
+
+
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
+    }
 }
